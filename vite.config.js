51ae7be6976f59247927ea19acec4c43696cba97
huangjiale724 <65434440+huangjiale724@users.noreply.github.com>
2023-06-20 15:51:33 +0800

    Add files via upload
    init

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+// const { resolve } = require("path");
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from "url";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      // "@": resolve(__dirname, "./src")
+      "@": fileURLToPath(new URL("./src", import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/assets/css/common.scss";`
+      }
+    }
+  }
+});
